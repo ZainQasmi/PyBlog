8a95331cacfde88810dd079662191291e6e45c2a
Zain Qasmi <zq@MacbookPro2.local>
2019-02-04 02:41:43 -0800

    first

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, flash, redirect, url_for, session, logging
+from data import Articles
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
+
+app = Flask(__name__)
+
+Articles = Articles()
+
+@app.route('/')
+
+def index():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html', articles = Articles)
+
+@app.route('/article/<string:id>/')
+def article(id):
+    return render_template('article.html', id=id)
+
+class RegisterForm(Form):
+    name = StringField('Name', [validators=.length])
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Tempor exercitation nulla excepteur consectetur dolor qui qui cupidatat sunt enim quis consequat deserunt veniam.',
+            'author': 'Alto Falto',
+            'create_data': '2019-02-04'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Tempor exercitation nulla excepteur consectetur dolor qui qui cupidatat sunt enim quis consequat deserunt veniam.',
+            'author': 'Alto Falto',
+            'create_data': '2019-02-04'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': 'Tempor exercitation nulla excepteur consectetur dolor qui qui cupidatat sunt enim quis consequat deserunt veniam.',
+            'author': 'Alto Falto',
+            'create_data': '2019-02-04'
+        },
+    ]
+    return articles
