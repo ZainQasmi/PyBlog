cdb9291171403e0a05804a49ea2acfb7ebc69826
Zain Qasmi <zainqasmi@gmail.com>
2019-02-05 23:15:53 -0800

    done

--- app.py
+++ app.py
@@ -176,7 +176,6 @@ def dashboard():
 	else:
 		msg = "No articles found"
 		return render_template('dashboard.html', msg=msg)
-
 	
 	# return render_template('dashboard.html')
 
@@ -209,6 +208,64 @@ def add_article():
 		flash('Article Created', 'success')
 		return redirect(url_for('dashboard'))
 	return render_template('add_article.html', form=form)
+
+# Edit Article
+@app.route('/edit_article/<string:id>/', methods=['GET', 'POST'])
+@is_user_logged_in
+def edit_article(id):
+	# Create Cursor
+	cur = mysql.connection.cursor()
+
+	# Get article by id
+	result = cur.execute("SELECT * FROM articles WHERE id=%s",[id])
+
+	article = cur.fetchone()
+
+	# Get form
+	form = ArticleForm(request.form)
+
+	# Populate article form fields
+	form.title.data = article['title']
+	form.body.data = article['body']
+
+	if request.method == 'POST' and form.validate():
+		title = request.form['title']
+		body = request.form['body']
+
+		# Create Cursor
+		cur = mysql.connection.cursor()
+
+		# Execute Query
+		cur.execute("UPDATE articles SET title=%s, body=%s WHERE id=%s", (title,body,id))
+
+		# Commit to DB
+		mysql.connection.commit()
+
+		# Close connection
+		cur.close()
+
+		flash('Article Updated', 'success')
+		return redirect(url_for('dashboard'))
+	return render_template('edit_article.html', form=form)
+
+# Delete Article
+@app.route('/delete_article/<string:id>/', methods=['POST'])
+@is_user_logged_in
+def delete_article(id):
+	# Create Cursor
+	cur = mysql.connection.cursor()
+
+	# Execute
+	cur.execute("DELETE FROM articles WHERE id=%s", [id])
+
+	# Commit to DB
+	mysql.connection.commit()
+
+	# Close connection
+	cur.close()
+
+	flash('Article Deleted', 'success')
+	return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
 	app.secret_key='secret123'
